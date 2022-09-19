340753742e9c4f8554f7033e3aac6477d72c99dd
Roberto Perez <roberto@robertoperez.me>
2022-09-19 16:49:44 -0400

    Add project files.

---
+++ comboNet4/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace comboNet4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboTest = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Test me";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboTest
+            // 
+            this.comboTest.AccessibleName = "Select an element";
+            this.comboTest.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboTest.FormattingEnabled = true;
+            this.comboTest.Items.AddRange(new object[] {
+            "Element 1",
+            "Element 2",
+            "Element 3"});
+            this.comboTest.Location = new System.Drawing.Point(16, 16);
+            this.comboTest.Name = "comboTest";
+            this.comboTest.Size = new System.Drawing.Size(121, 33);
+            this.comboTest.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboTest);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboTest;
+    }
+}
+
